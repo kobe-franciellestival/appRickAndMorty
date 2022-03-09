17893d64becc797a1de6c49ee517e2961891605e
Francielle Stival <francielle.stival@kobe.io>
2022-03-09 12:36:26 -0300

    teste revert

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class AppColors {
@@ -6,5 +7,5 @@ class AppColors {
   static Color appBarColor = const Color(0xff2A2831);
   static Color backgroundColor = Colors.black;
   static Color white = Colors.white;
-  static Color black = Colors.grey;
+  static Color black = Colors.black;
 }
