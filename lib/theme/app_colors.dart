198ec4bfdc5f9355f426115a5d918cf0cbb4bbea
Francielle Stival <francielle.stival@kobe.io>
2022-03-09 10:32:30 -0300

    style: alteracao da cor do app

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -5,7 +5,7 @@ class AppColors {
   static Color primaryColorDark = const Color(0xff5D78F0);
   static Color primaryColorLight = const Color(0xff87A1FA);
   static Color appBarColor = const Color(0xff2A2831);
-  static Color backgroundColor = Colors.black;
+  static Color backgroundColor = Colors.grey;
   static Color white = Colors.white;
   static Color black = Colors.black;
 }
